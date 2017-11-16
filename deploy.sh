ebd246621a774a5847e9646865a2441a5f9d7bcd
anton <akropp@gmail.com>
2017-11-16 10:50:06 -0800

    testing release finally

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 function snapshot() {
-  sbt -Dversion="1.0.${TRAVIS_BUILD_NUMBER}" publishSigned sonatypeRelease
+  sbt -Dversion="1.0.${TRAVIS_BUILD_NUMBER}" publishSigned sonatypeReleaseAll
 }
 
 function release() {
-  sbt -Dversion=$REVISION publishSigned sonatypeRelease
+  sbt -Dversion=$REVISION publishSigned sonatypeReleaseAll
 }
